78c97e389f37e7237788382dc09194861b550d34
Omid Bachari <omidbachari@gmail.com>
2016-04-08 14:33:50 -0400

    prep for semaphore

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :which_config, WhichConfig.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "which_config_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
