b147403fa8ea3653292853c667e7a01f664cf6c0
Omid Bachari <omidbachari@gmail.com>
2016-04-08 13:55:05 -0400

    changed db password in test

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :which_config, WhichConfig.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "which_config_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
